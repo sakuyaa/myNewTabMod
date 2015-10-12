4cdd66aaed001bd9461b906ef6c74a19ae72d357
sakuyaa <54321135@sina.com>
2015-10-12 22:17:59 +0800

    本地图标以斜杠开头

--- mynewtabmod@sakuyaa/myNewTabMod/data.js
+++ mynewtabmod@sakuyaa/myNewTabMod/data.js
@@ -2,9 +2,9 @@ var Config = getMStr(function() {
 	var sites;
 /*
 示例
-	项目地址, https://github.com/sakuyaa/myNewTabMod, ico/github.svg
-	卡饭, http://bbs.kafan.cn/forum.php?mod=forumdisplay&fid=215&filter=author&orderby=dateline, ico/kafan.ico
-	贴吧, http://tieba.baidu.com/f?ie=utf-8&kw=firefox, ico/baidu.svg
+	项目地址, https://github.com/sakuyaa/myNewTabMod, /ico/github.svg
+	卡饭, http://bbs.kafan.cn/forum.php?mod=forumdisplay&fid=215&filter=author&orderby=dateline, /ico/kafan.ico
+	贴吧, http://tieba.baidu.com/f?ie=utf-8&kw=firefox, /ico/baidu.svg
 	中文社区, https://www.firefox.net.cn/thread-29&orderby=postdate
 Yooo
 	王の教♂诲, http://www.bilibili.com/video/av1267
