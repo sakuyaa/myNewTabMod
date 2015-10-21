5a9a9a923f48b63a590e90281c59f5590f446190
sakuyaa <54321135@sina.com>
2015-10-21 13:42:01 +0800

    太长换成短网址了

--- mynewtabmod@sakuyaa/myNewTabMod/data.js
+++ mynewtabmod@sakuyaa/myNewTabMod/data.js
@@ -7,9 +7,9 @@ var Config = getMStr(function() {
 	贴吧, http://tieba.baidu.com/f?ie=utf-8&kw=firefox, /ico/baidu.svg
 	中文社区, https://www.firefox.net.cn/thread-29&orderby=postdate
 返利网站
-	天猫, http://s.click.taobao.com/t?e=m%3D2%26s%3DquL90fgDrCEcQipKwQzePCperVdZeJvipRe%2F8jaAHci5VBFTL4hn2e6JMB8hDV6F7km9mWjOCUbkQm6KluZECTEdm2IqHVtrn63f5lo3Jes0%2BJKlOUJJt%2BVKDnkZVfnBvoet87lGTGGIGrHgCgZNHPhXpSX4ya3U
-	聚划算, http://s.click.taobao.com/t?e=m%3D2%26s%3DVIWg%2Fb8lZhccQipKwQzePCperVdZeJviEViQ0P1Vf2kguMN8XjClArg2kbNAdLRF4Djo%2F5tdRB8FefUcJZkOuPpx%2Fm4tYT5Ocu1t1LOekWrWlp%2BRzhJZIqb6h%2BauVgdyCEDNGBpwx5jlSg55GVX5wbSqdkxH6LRFnaYpFBIfC%2F2orLd93QuCUMYOae24fhW0
-	阿里旅行, http://s.click.taobao.com/t?e=m%3D2%26s%3DGcBggZ3HsT4cQipKwQzePCperVdZeJviEViQ0P1Vf2kguMN8XjClArg2kbNAdLRFb32zYq%2Fv5KQFefUcJZkOuPpx%2Fm4tYT5Ocu1t1LOekWrWlp%2BRzhJZIpoTgr1w%2FQDpNgYL1oa%2FqkrlSg55GVX5wQpJxXZWmPBrLyo8ABgHx20aHks2%2FfPFu3EqY%2Bakgpmw
+	天猫, http://dwz.cn/24mb8g
+	聚划算, http://dwz.cn/24mcWP
+	阿里旅行, http://dwz.cn/24mdZQ
 Yooo
 	王の教♂诲, http://www.bilibili.com/video/av1267
 	Pump♂it, http://www.bilibili.com/video/av212109
