23bc5f5b734285ec8b9b4c107f91c8515b8ea3d6
sakuyaa <54321135@sina.com>
2015-10-21 13:01:01 +0800

    把calendar.min.js也移出去了
    简直就是丧心病狂啊

--- mynewtabmod@sakuyaa/chrome/content/index.js
+++ mynewtabmod@sakuyaa/chrome/content/index.js
@@ -27,6 +27,10 @@ var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = 'file:///' + encodeURI(dataFile.path.replace(/\\/g, '/')) + '/data.js';   //转为本地路径
 document.getElementsByTagName('head')[0].appendChild(script);
+script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'file:///' + encodeURI(dataFile.path.replace(/\\/g, '/')) + '/calendar.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
 var style = document.createElement('link');
 style.rel = 'stylesheet';
 style.type = 'text/css';
